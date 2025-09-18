4de234f1d9386ba05fdb303eb1563bc376f0efff
Swarup Sonawane <145706208+sswara18@users.noreply.github.com>
2025-09-18 17:59:12 +0530

    Distinct Number solution CSES using Python

--- distinctNumbers.py
+++ distinctNumbers.py
@@ -1,4 +1,33 @@
-n = int(input())
-numbers = list(map(int, input().split()))
-distict_values = set(numbers)
-print(len(distict_values))
+import sys
+input = sys.stdin.readline
+output = sys.stdout.write # must be string
+ 
+def hashing():
+    n = int(input())
+    arr_gen = (int(x) for x in input().split())
+    visit = set()
+    ans = 0
+    for num in arr_gen:
+        if num in visit:
+            continue
+ 
+        visit.add(num)
+        ans += 1
+ 
+    output(f"{ans}")
+ 
+ 
+def sorting():
+    n = int(input())
+    arr = sorted(int(x) for x in input().split())
+    ans = 1
+    for i in range(1, n):
+        if arr[i] != arr[i-1]:
+            ans += 1
+ 
+    output(f"{ans}")
+ 
+ 
+# hashing()
+sorting()
+
